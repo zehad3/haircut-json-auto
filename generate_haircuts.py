193e931acc6f6981f0f582da470dd8aa00061567
zehad3 <zehadfakir3@gmail.com>
2025-07-12 08:20:35 +0600

    Add files via upload

---
+++ generate_haircuts.py
@@ -0,0 +1,18 @@
+mport requests
+import json
+
+url = "https://google.serper.dev/images"
+s_key = os.getenv("SERPERAPI_KEY")
+payload = json.dumps({
+  "q": "man haircurt"
+})
+headers = {
+  'X-API-KEY': s_key,
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+data =  response.json()
+data = [{"url":img['imageUrl']} for img in data['images']]
+with open("public/haircuts.json", "w", encoding="utf8") as f:
+    json.dump(data, f, indent=2)
